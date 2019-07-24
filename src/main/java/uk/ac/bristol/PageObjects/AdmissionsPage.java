38d3af0d0690a9285930bb36a4d0caf05261f9a6
Chris Harris <chrisonefournine@hotmail.com>
2019-07-24 15:51:32 +0100

    abc

--- src/main/java/uk/ac/bristol/PageObjects/AdmissionsPage.java
+++ src/main/java/uk/ac/bristol/PageObjects/AdmissionsPage.java
@@ -11,6 +11,20 @@ public class AdmissionsPage extends BasePage {
     public static final By UNDERGRADUATE_LINK_IDENTIFIER = By.cssSelector("div.sv-col-md-6:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > a:nth-child(1)");
     public static final By ACD_LINK_IDENTIFIER = By.cssSelector("div.sv-col-md-6:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > a:nth-child(2)");
     public static final By RCD_LINK_IDENTIFIER = By.cssSelector("div.sv-col-md-6:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > a:nth-child(3)");
+    public static final By ALT_BULK_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(1) > div.sv-list-group.sv-portal-2-col > a:nth-child(4)");
+    public static final By UKCAT_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(1) > div.sv-list-group.sv-portal-2-col > a:nth-child(5)");
+    public static final By LNAT_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(1) > div.sv-list-group.sv-portal-2-col > a:nth-child(6)");
+    public static final By WORK_EXP_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(1) > div.sv-list-group.sv-portal-2-col > a:nth-child(7)");
+
+    public static final By OPEN_DAYS_TITLE_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(2) > div.sv-panel-heading > div h2");
+    public static final By TALK_SEARCH_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(2) > div.sv-list-group.sv-portal-2-col > a:nth-child(1)");
+    public static final By OPEN_DAY_SUMMARY_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(2) > div.sv-list-group.sv-portal-2-col > a:nth-child(2)");
+    public static final By ACCESS_REQUIREMENTS_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(2) > div.sv-list-group.sv-portal-2-col > a:nth-child(3)");
+
+    public static final By HELP_SUPPORT_TITLE_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(4) > div.sv-panel-heading > div > div > h2");
+    public static final By USER_GUIDES_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(4) > div.sv-list-group.sv-portal-2-col > a:nth-child(1)");
+    public static final By POLICY_GUIDENCE_LINK_IDENTIFIER = By.cssSelector("div:nth-child(1) > div > div > div:nth-child(4) > div.sv-list-group.sv-portal-2-col > a:nth-child(2)");
+
 
 
 
--- src/test/java/uk/ac/bristol/admissions/AdmissionsTest.java
+++ src/test/java/uk/ac/bristol/admissions/AdmissionsTest.java
@@ -28,6 +28,26 @@ public class AdmissionsTest extends BaseTest {
         assertThat(admissionsPage.isElementDisplayed(admissionsPage.UNDERGRADUATE_LINK_IDENTIFIER)).isTrue();
         assertThat(admissionsPage.isElementDisplayed(admissionsPage.ACD_LINK_IDENTIFIER)).isTrue();
         assertThat(admissionsPage.isElementDisplayed(admissionsPage.RCD_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.ALT_BULK_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.UKCAT_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.LNAT_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.WORK_EXP_LINK_IDENTIFIER)).isTrue();
+
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.OPEN_DAYS_TITLE_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.TALK_SEARCH_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.OPEN_DAY_SUMMARY_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.ACCESS_REQUIREMENTS_LINK_IDENTIFIER)).isTrue();
+
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.HELP_SUPPORT_TITLE_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.USER_GUIDES_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.POLICY_GUIDENCE_LINK_IDENTIFIER)).isTrue();
+
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.ATTRIBUTE_MANAGEMENT_TITLE_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.MANAGE_COURSE_ATTRIBUTES_LINK_IDENTIFIER)).isTrue();
+        assertThat(admissionsPage.isElementDisplayed(admissionsPage.MANAGE_ADMISSIONS_THRESHOLDS_LINK_IDENTIFIER)).isTrue();
+
+
+
 
 
     }
