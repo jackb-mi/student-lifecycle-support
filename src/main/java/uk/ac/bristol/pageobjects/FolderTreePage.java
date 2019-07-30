25e63b564dea82c25ede8e4c59f27c97585e45ab
Chris Harris <chrisonefournine@hotmail.com>
2019-07-30 16:47:33 +0100

    Extended test to cover multiple folder checks.
    Added some more locators to FolderTreePage for additional columns
    
    Added correct selectors to the enum

--- src/main/java/uk/ac/bristol/pageobjects/FolderTreePage.java
+++ src/main/java/uk/ac/bristol/pageobjects/FolderTreePage.java
@@ -20,6 +20,9 @@ public class FolderTreePage extends BasePage {
     public static final By COLUMN_THIRTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(14)");
     public static final By COLUMN_FOURTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(15)");
     public static final By COLUMN_FIFTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(16)");
+    public static final By COLUMN_SIXTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(17)");
+    public static final By COLUMN_SEVENTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(18)");
+    public static final By COLUMN_EIGHTNTEEN_IDENTIFIER = By.cssSelector("#headrow > th:nth-child(19)");
 
     public FolderTreePage(ChromeDriver driver) {
         super(driver);
