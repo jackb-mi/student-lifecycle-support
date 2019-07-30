d6ec4a3acacdd1089b9374887c5e45a77915079e
jackb-mi <jack.ball@mosaicisland.co.uk>
2019-07-30 15:20:22 +0100

    Update with FolderTree test changes

--- src/main/java/uk/ac/bristol/pageobjects/HomePage.java
+++ src/main/java/uk/ac/bristol/pageobjects/HomePage.java
@@ -1,14 +1,11 @@
 package uk.ac.bristol.pageobjects;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.fail;
-
-
 public class HomePage extends BasePage {
-
     private static final By ADMISSIONS_LINK_IDENTIFIER = By.cssSelector("body > main > div > div.sv-row > div > div > div.sv-list-group > div:nth-child(1) > a");
     private static final By REGISTRATIONS_LINK_IDENTIFIER = By.cssSelector("#REG001");
 
@@ -17,26 +14,13 @@ public class HomePage extends BasePage {
     }
 
     public AdmissionsPage clickAdmissionsLink() {
-
         try {
             driver.findElement(ADMISSIONS_LINK_IDENTIFIER).click();
-        } catch (NoSuchElementException e) {
-            fail("Admissions Link Not Found");
+        } catch (NoSuchElementException var2) {
+            Assert.fail("Admissions Link Not Found");
         }
 
         AdmissionsPage admissionsPage = new AdmissionsPage(driver);
         return admissionsPage;
-    }
-
-    public RegistrationPage clickRegistrationsLink() {
-
-        try {
-            driver.findElement(REGISTRATIONS_LINK_IDENTIFIER).click();
-        } catch (NoSuchElementException e) {
-            fail("Registrations Link Not Found");
-        }
-
-        RegistrationPage registrationPage = new RegistrationPage(driver);
-        return registrationPage;
     }
 }
--- src/test/java/uk/ac/bristol/BaseTest.java
+++ src/test/java/uk/ac/bristol/BaseTest.java
@@ -1,17 +1,23 @@
 package uk.ac.bristol;
 
+import com.jayway.awaitility.Awaitility;
+import com.jayway.awaitility.core.ConditionTimeoutException;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import uk.ac.bristol.pageobjects.HomePage;
 import uk.ac.bristol.pageobjects.LoginPage;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-
     public ChromeDriver driver;
+    public By PAGE_TITLE_IDENTIFIER = By.cssSelector("#sitsportalpagetitle");
 
     @BeforeMethod
     public void beforeMethod(Method method) {
@@ -57,5 +63,25 @@ public class BaseTest {
         loginPage.enterUsernameAndPassword(username, password);
         HomePage homePage = loginPage.loginWithValidCredentials();
         return homePage;
+    }
+
+    public Boolean isElementDisplayed(By elementId) {
+        return this.driver.findElements(elementId).size() > 0;
+    }
+
+    public String getPageTitle() {
+        try {
+            Awaitility.await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+                return this.isElementDisplayed(this.PAGE_TITLE_IDENTIFIER);
+            });
+        } catch (ConditionTimeoutException var2) {
+            Assert.fail("Page Title Not Found");
+        }
+
+        return this.getTextFromElement(this.PAGE_TITLE_IDENTIFIER);
+    }
+
+    public String getTextFromElement(By elementId) {
+        return this.driver.findElement(elementId).getText();
     }
 }
