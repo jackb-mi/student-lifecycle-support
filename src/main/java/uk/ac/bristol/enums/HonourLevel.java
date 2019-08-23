9f92a85f95a75edc85cd62a4546d9a04292d62bb
jackb-mi <jack.ball@mosaicisland.co.uk>
2019-08-23 15:04:05 +0100

    undo commented out assert on admissions test and correct typo in comment

--- src/main/java/uk/ac/bristol/enums/HonourLevel.java
+++ src/main/java/uk/ac/bristol/enums/HonourLevel.java
@@ -6,7 +6,7 @@ public enum HonourLevel {
     OVERSEAS(),
 
     /* NOT_APPLICABLE has been included for use when creating Programmes in Curriculum Management,
-    An honor level may of may not required depending on the Programme Type being created.
+    An honor level may or may not required depending on the Programme Type being created.
     This option should be used when methods may or may not perform functions with the HonorLevel enum */
     NOT_APPLICABLE;
 
