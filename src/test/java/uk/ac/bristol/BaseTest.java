283dde08aa2c68c3f3aa2bacbf03f1bb32fcceab
jackb-mi <jack.ball@mosaicisland.co.uk>
2019-09-04 10:25:32 +0100

    chromedriver location

--- src/test/java/uk/ac/bristol/BaseTest.java
+++ src/test/java/uk/ac/bristol/BaseTest.java
@@ -28,7 +28,7 @@ public class BaseTest {
     }
 
     private void instantiateChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
 //        driver = webdriver.Chrome('/path/to/your_chrome_driver_dir/chromedriver',chrome_options=chrome_options)
 
@@ -41,6 +41,7 @@ public class BaseTest {
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
+
 
         this.driver = new ChromeDriver(chromeOptions);
 
