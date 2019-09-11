b733ed699283a78101627debeb176e34a9a92470
Chris Harris <chrisonefournine@hotmail.com>
2019-09-11 13:59:29 +0100

    Added some more selectors to the ACD homepage

--- src/main/java/uk/ac/bristol/pageobjects/admissions/ACD.java
+++ src/main/java/uk/ac/bristol/pageobjects/admissions/ACD.java
@@ -10,6 +10,7 @@ public class ACD extends BasePage {
     public static final String PAGE_TITLE = "Adpplicantion Clearance & Decision Entry";
 
     public static final By PREVIOUS_BUTTON_IDENTIFIER = By.xpath("//div[contains(text(), 'Upload')]");
+    public static final By FIRST_MESSAGE_IDENTIFIER = By.xpath("//*[@id='panel-ACD-HEAD']//strong/text()[1]");
     public static final By NEXT_BUTTON_IDENTIFIER = By.xpath("//div[contains(text(), 'alternative')]");
     public static final By SEARCH_BUTTON_IDENTIFIFER = By.xpath("//div[contains(text(), 'Request')]");
     public static final By RELOAD_BUTTON_IDENTIFIER = By.xpath("//div[contains(text(), 'In care')]");
@@ -22,5 +23,42 @@ public class ACD extends BasePage {
     public static final By DECISION_TAB_IDENTIFIER = By.xpath("//div[contains(text(), 'Request')]");
     public static final By ACTUAL_QUALS_TAB_IDENTIFIER = By.xpath("//div[contains(text(), 'Request')]");
     public static final By APPLICANTION_LOG_TAB_IDENTIFIER = By.xpath("//div[contains(text(), 'Request')]");
+
+    public class SearchPanel {
+        //***Need to change selector for these***
+        //public static final By SEARCH_CRITERIA_HEADER_IDENTIFIER = By.className("sv-panel-title");
+        //public static final By SEARCH_RESULTS_HEADER_IDENTIFIER = By.className("sv-panel-title");
+        public static final By STUDENT_CODE_FIELD_IDENTIFIER = By.cssSelector("#CAP_STUC");
+        public static final By UCAS_ID_FIELD_IDENTIFIER = By.cssSelector("#CAP_UCI2");
+        public static final By FORENAME_FIELD_IDENTIFIER = By.id("STU_FNM1");
+        public static final By SURNAME_FIELD_IDENTIFIER = By.id("STU_SURN");
+        public static final By RETRIEVE_BUTTON_IDENTIFIER = By.id("FINDAPP");
+        public static final By STUCODE_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By CAPSEQN_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By APFSEQN_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By DOB_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By NAME_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By PERSONALID_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By GENDER_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By DECISION_RESPONSE_IDENTIFIER = By.id("FINDAPP");
+        public static final By SELECT_COLUMN_IDENTIFIER = By.id("FINDAPP");
+        public static final By SELECT_BUTTON_IDENTIFIER = By.id("0_SELECT");
+        public static final By CLEAR_BUTTON_IDENTIFIER = By.cssSelector(".ui-helper-clearfix > div > button:nth-child(1)T");
+        public static final By CLOSE_BUTTON_IDENTIFIER = By.cssSelector(".ui-helper-clearfix > div > button:nth-child(2)");
+    }
+
+    public class ApplicantDetails {
+        public static final By PERSONAL_DETAILS_HEADER_IDENTIFIER = By.cssSelector("#ycls-tab-UDT3 > div > div.sv-row > div:nth-child(1) > div > div > div > div > h3");
+
+
+    }
+
+
+
+
+
+
+
+
 
 }
